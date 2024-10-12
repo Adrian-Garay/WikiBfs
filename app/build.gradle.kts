a8fa5b415b3d7ee2bafaf15e4c91f19aee321b72
AdrianGaray <99428110+Adrian-Garay@users.noreply.github.com>
2024-10-12 20:56:47 +0200

    Reinicio del proyecto con hola mundo funcional

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.22"
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
     id("application")
 }
 
@@ -8,17 +8,35 @@ repositories {
 }
 
 dependencies {
+    //Tests inutiles
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     implementation("com.google.guava:guava:31.1-jre")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
 }
 
 application {
-    mainClass.set("wikibfs.AppKt")
+    mainClass.set("org/example/AppKt")
+}
+
+// JVM Toolchain
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(22))
+    }
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "org/example/AppKt"
+        )
+    }
 }
 
 tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
